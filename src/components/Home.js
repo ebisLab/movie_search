1374da75d5106706d060a8b1209b77100e1edb34
ebisLab <ebaiden@techie.com>
2021-01-08 19:10:39 -0500

    cards created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function Home({movies}) {
+export default function Home({movies,addItem}) {
+    
     
     return (
         <div>
@@ -11,7 +12,11 @@ export default function Home({movies}) {
             </form>
         <div style={{display:"inline-flex"}}>
             
-            {movies.map(item=><div key={item.id} style={{margin:"10px", border:"1px solid black", height:"100px", width:"100px"}}>{item.name}</div>)}
+            {movies && movies.map(item=><div key={item.id} style={{margin:"10px", border:"1px solid black", height:"100px", width:"100px", textAlign:"-webkit-center"}}><div style={{height:"100%", position:"relative"}}>{item.name}
+            
+            <div style={{border:"1px solid black", height:"20px",width:"40px", position:"absolute", bottom:0, left:"50%", transform: "translateX(-50%)", cursor:"pointer", marginBottom:"15px"}}
+            onClick={()=>addItem(item)}
+            >Add</div></div></div>)}
         </div>
         </div>
     )
